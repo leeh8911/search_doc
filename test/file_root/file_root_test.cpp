fc426616277898d9d2aee94cbcb6fd9806be4e0a
sangwon <leeh8911@gmail.com>
2022-12-18 22:35:16 +0000

    update file_root_test
    replace test_file.md to raii file class

--- test/file_root/file_root_test.cpp
+++ test/file_root/file_root_test.cpp
@@ -13,31 +13,44 @@
 #include <gtest/gtest.h>
 
 #include <filesystem>
+#include <fstream>
 
 #include "src/value_object/document.h"
+#include "test/util/raii_file.h"
 
-namespace search_doc::domain::test {
+namespace search_doc::test {
+namespace fs = std::filesystem;
 
 TEST(ActualFileRootTest, TotalFilesCase) {
-    ActualFileRoot sut("../test/file_root/test_root", {".md"});
+    util::RAIIFile file("dummy.dtxt");
+    file << "Test "
+         << "File";
+
+    fs::path file_path = file.Path();
+
+    domain::ActualFileRoot sut(file_path.parent_path().string(), {file_path.extension()});
 
     auto doc_list = sut.Search();
 
     EXPECT_EQ(1, doc_list.size());
 
-    std::filesystem::directory_entry expect_entry("../test/file_root/test_root/test_file.md");
+    std::filesystem::directory_entry expect_entry(file.Path());
     EXPECT_EQ(value_object::Document(expect_entry), *doc_list.front());
 }
 
 TEST(ActualFileRootTest, KeywordCheckCase) {
-    ActualFileRoot sut("../test/file_root/test_root", {".md"});
+    util::RAIIFile file("dummy.dtxt");
+    file << "Test "
+         << "File";
+
+    fs::path file_path = file.Path();
+
+    domain::ActualFileRoot sut(file_path.parent_path().string(), {file_path.extension()});
 
     auto doc_list = sut.Search();
 
     auto doc = doc_list.front();
     EXPECT_TRUE(doc->Contains("Test")) << *doc;
     EXPECT_TRUE(doc->Contains("File")) << *doc;
-
-    std::cout << *doc << "\n";
 }
-}  // namespace search_doc::domain::test
+}  // namespace search_doc::test
